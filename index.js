0c9f4ee699da87a1a7748f3592d5766d26e1f1ec
KittipopPhetburi <s6506021620016@email.kmutnb.ac.th>
2024-02-23 03:24:34 +0700

    Input Data

--- index.js
+++ index.js
@@ -1,26 +1,56 @@
+require("dotenv").config();
+
 const express = require('express');
 const sqlite3 = require('sqlite3');
 const app = express();
 
-const db = new sqlite3.Database('./Database/Books.sqlite');
+const db = new sqlite3.Database('./Database/Musics.sqlite');
 
 app.use(express.json());
 
-db.run(`CREATE TABLE IF NOT EXISTS books (
+db.run(`CREATE TABLE IF NOT EXISTS music (
     id INTEGER PRIMARY KEY,
     title TEXT,
-    author TEXT
+    singer TEXT,
+    genre TEXT,
+    release_date TEXT
 )`);
 
-app.get('/books', (req,res) => {
-    db.all(`SELECT * FROM books`, (err,rows) => {
+db.run(`CREATE TABLE IF NOT EXISTS user (
+    user_id INTEGER PRIMARY KEY,
+    username TEXT,
+    password TEXT,
+    name TEXT,
+    age TEXT,
+    phone_number CHAR
+)`);
+
+db.run(`CREATE TABLE IF NOT EXISTS playlist (
+    playlist_id INTEGER PRIMARY KEY,
+    playlistname TEXT,
+    music_id INTEGER,
+    titke TEXT,
+    singer TEXT,
+    user_id INTEGER
+)`);
+
+db.run(`CREATE TABLE IF NOT EXISTS review (
+    review_id INTEGER PRIMARY KEY,
+    music_id INTEGER,
+    user_id INTEGER,
+    score DOUBLE,
+    comment TEXT
+)`);
+
+app.get('/music', (req,res) => {
+    db.all(`SELECT * FROM music`, (err,rows) => {
         if (err) res.status(500).send(err);
         else res.json(rows);
     });
 });
 
-app.get('/books/:id', (req,res) => {
-    db.get(`SELECT * FROM books WHERE id = ?`, req.params.id, (err,rows) => {
+app.get('/music/:id', (req,res) => {
+    db.get(`SELECT * FROM music WHERE id = ?`, req.params.id, (err,rows) => {
         if (err) res.status(500).send(err);
         else {
             if (!rows) res.status(404).send('Book not found');
@@ -29,31 +59,31 @@ app.get('/books/:id', (req,res) => {
     });
 });
 
-app.post('/books', (req,res) => {
-    const book = req.body;
-    db.run(`INSERT INTO books (title,author) VALUES (?,?)`, book.title, book.author, function(err) {
+app.post('/music', (req,res) => {
+    const music = req.body;
+    db.run(`INSERT INTO music (title,author) VALUES (?,?)`, music.title, music.author, function(err) {
         if (err) res.status(500).send(err);
         else {
-            book.id = this.lastID;
-            res.send(book);
+            music.id = this.lastID;
+            res.send(music);
         }
     });
 });
 
-app.put('/books/:id', (req,res) => {
-    const book = req.body;
-    db.run(`UPDATE books SET title = ?, author = ? WHERE id = ?`, book.title, book.author, req.params.id, function(err) {
+app.put('/music/:id', (req,res) => {
+    const music = req.body;
+    db.run(`UPDATE music SET title = ?, author = ? WHERE id = ?`, music.title, music.author, req.params.id, function(err) {
         if (err) res.status(500).send(err);
-        else res.send(book);
+        else res.send(music);
     });
 });
 
-app.delete('/books/:id', (req,res) => {
-    db.run(`DELETE FROM books WHERE id = ?`, req.params.id, function(err) {                 
+app.delete('/music/:id', (req,res) => {
+    db.run(`DELETE FROM music WHERE id = ?`, req.params.id, function(err) {
         if (err) res.status(500).send(err);
         else res.send({});
     });
 });
 
-const port = process.env.PORT || 8000;
-app.listen(port, () => console.log(`Listening on port http://localhost/${port}...`));
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port http://localhost:${port}...`));
